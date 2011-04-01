fe338138b7627144e5b29162303476a8b7b83b77
Thomas Backman <serenity@exscape.org>
2011-04-01 18:16:11 +0200

    Add the full RegexKit license text to the about window to be safe (and nice)

--- Lyricus/MainController.m
+++ Lyricus/MainController.m
@@ -219,6 +219,23 @@
 	 @"  http://regexkit.sourceforge.net"];
 }
 
+-(IBAction)showRegexKitLicense:(id)sender {
+	NSString *licenseString = 
+	@"Copyright © 2007-2008, John Engelhart\n"
+	@"\n"
+	 @"All rights reserved.\n"
+	 @"\n"
+	 @"Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:\n"
+	 @"\n"
+	 @"* Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.\n"
+	 @"* Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.\n"
+	 @"* Neither the name of the Zang Industries nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.\n"
+	 @"THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.";
+	
+	[[NSAlert alertWithMessageText:@"Lyricus uses the RegexKitLite library by John Engelhart." defaultButton:@"OK" alternateButton:nil otherButton:nil informativeTextWithFormat:licenseString] runModal];
+	 
+}
+
 -(IBAction) followiTunesCheckboxClicked:(id) sender {
 	if ([followiTunesCheckbox state] == NSOnState) {
 		if ([helper isiTunesRunning]) {
@@ -376,7 +393,7 @@
         return;
 	}
 	
-	if ([[editModeMenuItem title] isEqualToString:@"Leave edit mode"]) {
+	if ([[editModeMenuItem title] isEqualToString:@"Leave edit mode"] ) {
 		// Edit mode is active
 		trackChangedWhileInEditMode = YES;
 		// Don't change the displayed lyrics while editing
