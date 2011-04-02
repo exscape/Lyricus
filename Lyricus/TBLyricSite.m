c00b2d0b68a3cedc3d08b55b3e03209045359e00
Thomas Backman <serenity@exscape.org>
2011-04-02 18:31:45 +0200

    Add copyright header to TBLyricSite

--- Lyricus/TBLyricSite.m
+++ Lyricus/TBLyricSite.m
@@ -1,9 +1,7 @@
 //
-//  TBLyricClass.m
-//  Lyricus
-//
-//  Created by Thomas Backman on 3/26/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// This file is part of Lyricus.
+// Copyright (c) 2008-2011, Thomas Backman <serenity@exscape.org>
+// This software is distributed under the terms of the MIT license. For details, see license.txt.
 //
 
 #import "TBLyricSite.h"
