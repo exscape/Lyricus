53fe614c29096525f3f5e28f585f11a1269de1e2
Thomas Backman <serenity@exscape.org>
2011-04-03 13:07:36 +0200

    Add copyrights to LyricTextView; group a few files

--- Lyricus/LyricTextView.m
+++ Lyricus/LyricTextView.m
@@ -1,9 +1,7 @@
 //
-//  LyricTextView.m
-//  Lyricus
-//
-//  Created by Thomas Backman on 4/2/11.
-//  Copyright 2011 __MyCompanyName__. All rights reserved.
+// This file is part of Lyricus.
+// Copyright (c) 2008-2011, Thomas Backman <serenity@exscape.org>
+// This software is distributed under the terms of the MIT license. For details, see license.txt.
 //
 
 #import "LyricTextView.h"
