8e140f9fd9efc718be99e78d375c3c6075cbd35e
Thomas Backman <serenity@exscape.org>
2012-07-14 17:33:16 +0200

    Fix Songmeanings support, since their big site update recently. Also, make the outdated index message slightly less condescending.

--- Lyricus/LyricSearchController.m
+++ Lyricus/LyricSearchController.m
@@ -73,7 +73,7 @@
         
 		/* Warn if older than one week */
         if (diff > 86400*7 && ! [[NSUserDefaults standardUserDefaults] boolForKey:@"Disable cache warning"] && !tempIgnoreCheckIndex) {
-			NSInteger ret = [[NSAlert alertWithMessageText:@"The lyric index is out-of-date." defaultButton:@"Update Index Now" alternateButton:@"Ignore" otherButton:nil informativeTextWithFormat:@"Your lyric index is more than one week old. If you have added, removed or changed tracks or lyrics since then, the results will be out-of date. Please update your index."] runModal];
+			NSInteger ret = [[NSAlert alertWithMessageText:@"The lyric index is out-of-date." defaultButton:@"Update Index Now" alternateButton:@"Ignore" otherButton:nil informativeTextWithFormat:@"Your lyric index is more than one week old. If you have added, removed or changed tracks or lyrics since then, the results will be out-of date."] runModal];
             if (ret == NSAlertDefaultReturn) {
                 [self updateTrackIndex:self];
 				tempIgnoreCheckIndex = NO;
--- Lyricus/TBSongmeanings.m
+++ Lyricus/TBSongmeanings.m
@@ -72,7 +72,7 @@
 	
 	NSString *artist = [inArtist stringByReplacingOccurrencesOfString:@" " withString:@"+"];
 	artist = [artist stringByAddingPercentEscapesUsingEncoding:NSISOLatin1StringEncoding];
-    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"http://www.songmeanings.net/query/?q=%@&type=artists&page=1&start=0&mm=1&pp=20&b=Search", artist]];
+    NSURL *url = [NSURL URLWithString: [NSString stringWithFormat:@"http://www.songmeanings.net/query/?query=%@&type=artists", artist]];
 
 	// Do the search and fetch results
     NSError *err = nil;
@@ -89,21 +89,33 @@
         return nil;
 	}
 
-	if ([html containsString:@"There are <strong>no results</strong>"])
+	if ([html containsString:@"Your query turned up no results."])
 		return nil;
-	// else continue...
-
-
-    NSString *regex = 
-    @"<div class=\"row-left\">\\s*<a href=\"http://www.songmeanings.net/artist/view/songs/(\\d+)/\"><strong>(.*?)</strong></a>\\s*</div>";
- 	
-    NSArray *matchArray = [html arrayOfDictionariesByMatchingRegex:regex withKeysAndCaptures:@"id", 1, @"artist", 2, nil];
-
-    for (NSDictionary *match in matchArray) {
-		if ([inArtist isEqualToCharactersInString:[match objectForKey:@"artist"] ignoringCase:YES])
-			return [NSString stringWithFormat:@"http://www.songmeanings.net/artist/view/songs/%@/", [match objectForKey:@"id"]];
+	
+	NSString *regex = nil; /* See below */
+	
+	if ([html containsString:@"<tbody id=\"songslist\">"]) {
+		// Yay, we went on directly to the song list. Unfortunately, we don't know the URL to the page
+		// we were redirected to, without modifications to the HTML fetcher... We can extract it from the page,
+		// however; there are links right back to where we are.
+		regex = @"href=\"/artist/view/songs/(\\d+)";
+		
 	}
-
+	else {
+		// We're on an artist name disambiguation page, not the actual artist page
+		// where the song list is. Try to extract the correct URL to the artist page.
+		regex = [NSString stringWithFormat:@"href=\"/artist/view/songs/(\\d+)/\" title=\"%@\"", inArtist];
+	
+	}
+	
+	// Either way, this code is the same:
+	
+	NSArray *matchArray = [html arrayOfDictionariesByMatchingRegex:regex options:RKLCaseless range:NSMakeRange(0UL,[html length]) error:NULL withKeysAndCaptures:@"id", 1, nil];
+	
+	for (NSDictionary *match in matchArray) {
+		return [NSString stringWithFormat:@"http://www.songmeanings.net/artist/view/songs/%@/", [match objectForKey:@"id"]];
+	}
+	
 	return nil;
 }
 
@@ -127,7 +139,8 @@
     }
 	
     NSString *regex = 
-      @"<tr class='row[01]'><td><a href=\"/songs/view/(\\d+)/\">(.*?)</a></td>";
+	//      @"<tr class='row[01]'><td><a href=\"/songs/view/(\\d+)/\">(.*?)</a></td>";
+	@"href=\"/songs/view/(\\d+)/\" title=\"[^\"]*\"\\s*>\\s*([^<]+)</a>";
     
 	NSArray *matchArray = [html arrayOfDictionariesByMatchingRegex:regex withKeysAndCaptures:@"id", 1, @"title",2, nil];
     for (NSDictionary *match in matchArray) {
@@ -169,9 +182,10 @@
 	NSString *regex = 
 	//	@"<!-- end ringtones -->([\\s\\S]*?)<!--ringtones and media links -->";
 	//@"<div id=\"songText2\" style=\"font-size: 11px;\"class=\"protected\">\\s*\r\n(?:<script type=\"text/javascript\">[\\s\\S]+?</script>)?([\\s\\S]*?)(?:<br/>---<br/>\"[\\s\\S]+\" as written|\\s*<!--ringtones and media links -->)";
-	@"<div id=\"songText2\" style=\"font-size: 11px;\"class=\"protected\">\\s*([\\s\\S]*?)(?:<br/>---<br/>\"[\\s\\S]+\" as written|\\s*<!--ringtones and media links -->)";
+	//@"<div id=\"songText2\" style=\"font-size: 11px;\"class=\"protected\">\\s*([\\s\\S]*?)(?:<br/>---<br/>\"[\\s\\S]+\" as written|\\s*<!--ringtones and media links -->)";
+	@"<div id=\"lyricsblock.{0,3}\">(.*?)</div>"; // At last, an update that makes life *easier*!
 	
-	NSMutableString *lyrics = [[html stringByMatching:regex capture:1L] mutableCopy];
+	NSMutableString *lyrics =[[html stringByMatching:regex options:RKLDotAll inRange:NSMakeRange(0L, [html length]) capture:1L error:NULL] mutableCopy];
 
     if (lyrics == nil) {
         NSMutableDictionary *errorDetail = [NSMutableDictionary dictionary];
